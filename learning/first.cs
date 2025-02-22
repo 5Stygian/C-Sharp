42d8e03ec063404c6c89e15e645ec8fa9b910405
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-02-22 14:26:17 -0800

    Update first.cs

--- learning/first.cs
+++ learning/first.cs
@@ -1,41 +1,43 @@
 using System;
 
-namespace bm
-{
-        class class1
-        {
-                static void Main(string[] args) 
-                {
-                        Console.WriteLine("Enter first number: ");
-                        string num1in = Console.ReadLine();
-                        Console.WriteLine("Enter second number: ");
-                        string num2in = Console.ReadLine();
-                        double num1 = Convert.ToDouble(num1in);
-                        double num2 = Convert.ToDouble(num2in);
+namespace bm {
+        class class1 {
+                static void Main(string[] args) {       
+                        static void input() {
+                                Console.Write("Enter first number: ");
+                                string num1in = Console.ReadLine();
+                                Console.Write("Enter second number: ");
+                                string num2in = Console.ReadLine();
+                                double num1 = Convert.ToDouble(num1in);
+				double num2 = Convert.ToDouble(num2in);
 
-                        Console.WriteLine("\n 1 - Addition \n 2 - Subtraction \n 3 - Multiplication \n 4 - Division \n");
-                        Console.WriteLine("Basic arithmatic operation: ");
-                        string operationin = Console.ReadLine();
-                        int operation = Convert.ToInt16(operationin);
+				Console.WriteLine("\n 1 - Addition \n 2 - Subtraction \n 3 - Multiplication \n 4 - Division \n");
+                                Console.Write("Basic arithmatic operation: ");
+                                string operation_in = Console.ReadLine();
+                                int operation = sbyte.Parse(operation_in);
 
-                        switch (operation)
-                        {
-                                case 1:
-                                        Console.WriteLine($"{num1} + {num2} = " + (num1 + num2));
-                                        break;
-                                case 2:
-                                        Console.WriteLine($"{num1} - {num2} = " + (num1 - num2));
-                                        break;
-                                case 3:
-                                        Console.WriteLine($"{num1} * {num2} = " + (num1 * num2));
-                                        break;
-                                case 4:
-                                        Console.WriteLine($"{num1} / {num2} = " + (num1 / num2));
-                                        break;
-                                default:
-                                        Console.WriteLine("Invalid input");
-                                        break;
-                        }
-                }
+				switch (operation) {
+					case 1:
+						Console.WriteLine($"{num1} + {num2} = " + (num1 + num2));
+						break;
+					case 2:
+						Console.WriteLine($"{num1} - {num2} = " + (num1 - num2));
+						break;
+					case 3:
+						Console.WriteLine($"{num1} * {num2} = " + (num1 * num2));
+						break;
+					case 4:
+						Console.WriteLine($"{num1} / {num2} = " + (num1 / num2));
+						break;
+					default:
+						Console.WriteLine("Invalid operation");
+						input(); // recursive function call that would make NASA's guidelines cry 
+						break;
+				}
+			}
+
+			// call input function
+			input();
+		}
         }
 }
